d97ef2415229aabc240f18aff99071b311131bf4
Sinclair <sinclairolajuwon@gmail.com>
2024-10-02 16:14:37 -0500

    not done

---
+++ contracts/SaveERC20.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT LICENSE
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SaveERC20 {
+
+    error AddressZeroDetected();
+    error CantSendZero();
+    error InsufficientFunds();
+    error NotOwner();
+    error CantSendToZeroAddress();
+
+    address tokenAddress;
+    address _owner;
+
+    constructor (address _tokenAddress){
+        _owner = msg.sender;
+        tokenAddress = _tokenAddress;
+    }
+
+    mapping (address => uint256) balance;
+    event withdrawalSuccessful (address indexed user, uint256 indexed amount);
+    event depositSuccessful (address indexed user, uint256 indexed amount);
+    event depositForAnotherUserSuccesful  (address indexed sender,address indexed user, uint256 indexed amount);
+
+    function deposit (uint256 amount) external{
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        if(IERC20(tokenAddress).balanceOf(msg.sender) < amount){revert InsufficientFunds();}
+        if(amount <= 0) {revert CantSendZero();}
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
+        balance[msg.sender] += amount;
+        emit depositSuccessful(msg.sender, amount);
+    }
+
+    function myBalance() external view returns(uint256){
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        return balance[msg.sender];
+    }
+
+    function contractBalance() external view returns(uint256){
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        if(msg.sender != _owner){revert NotOwner();}
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function userBalance(address _user) external view returns(uint256){
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        require(msg.sender == _owner,"Not authorized");
+        return balance[_user];
+    }
+
+    function withdraw(uint256 amount) external{
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+       if(balance[msg.sender] <= amount) {revert InsufficientFunds();}
+        if(amount <= 0) {revert CantSendZero();}
+        balance[msg.sender] -= amount;
+        IERC20(tokenAddress).transfer(msg.sender, amount);
+    }
+
+    function transerFunds(uint256 amount, address _to) external{
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        if(_to == address(0)){revert CantSendToZeroAddress();}
+        if(balance[msg.sender] <= amount) {revert InsufficientFunds();}
+         if(amount <= 0) {revert CantSendZero();}
+        balance[msg.sender] -= amount;
+
+        IERC20(tokenAddress).transfer(_to, amount);
+    }
+
+    function depositForAnotherUser(uint256 amount, address _to) external{
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        if(_to == address(0)){revert CantSendToZeroAddress();}
+         if(amount <= 0) {revert CantSendZero();}
+        
+        if(IERC20(tokenAddress).balanceOf(msg.sender) <amount){revert InsufficientFunds();}
+
+        IERC20(tokenAddress).transferFrom(msg.sender, _to, amount);
+
+        balance[_to] += amount;
+
+        emit depositForAnotherUserSuccesful(msg.sender, _to, amount);
+
+
+
+    }
+
+}
---
+++ contracts/StakeERC20.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT LICENSE
+pragma solidity ^0.8.27;
+
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract StakeERC20 {
+
+    error AddressZeroDetected();
+    error CantSendZero();
+    error InsufficientFunds();
+    error NotStaked();
+    error NotOwner();
+    error TimeNotReached();
+    error AlreadyWithdrawn();
+    error AlreadyStaked();
+    
+    uint256 totalStaked;
+    address tokenAddress;
+    address owner;
+
+    struct Stake{
+        bool isStaked;
+        uint256 amount;
+        uint256 period;
+        uint256 startTime;
+    }
+
+    mapping (address => Stake) stakers;
+
+    constructor (address _tokenAddress){
+        tokenAddress = _tokenAddress;
+        owner = msg.sender;
+    }
+
+    function stake(uint256 _periodInDays, uint256 amount ) external {
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        if(IERC20(tokenAddress).balanceOf(msg.sender) < amount){revert InsufficientFunds();}
+        if(amount <= 0) {revert CantSendZero();}
+        if (stakers[msg.sender].isStaked == true) { revert AlreadyStaked(); }
+
+        IERC20(tokenAddress).transferFrom(msg.sender,address(this),amount);
+        totalStaked += amount;
+        
+        stakers[msg.sender] = Stake(true,amount,block.timestamp + _periodInDays * 1 days,block.timestamp);
+    }
+
+    function withdraw() external {
+        if(block.timestamp < stakers[msg.sender].period){revert TimeNotReached();}
+        if(stakers[msg.sender].isStaked == false){revert AlreadyWithdrawn();}
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+
+        uint256 amount = stakers[msg.sender].amount + calcReward();
+
+        totalStaked -= stakers[msg.sender].amount;
+
+        stakers[msg.sender].amount = 0; 
+        stakers[msg.sender].isStaked = false;
+
+        IERC20(tokenAddress).transfer(msg.sender, amount);
+    }
+
+    function emergencyWithdraw () external {
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        if(stakers[msg.sender].isStaked == false){revert AlreadyWithdrawn();}
+
+        uint256 amount = stakers[msg.sender].amount;
+
+        totalStaked -= stakers[msg.sender].amount;
+
+        stakers[msg.sender].amount = 0; 
+        stakers[msg.sender].isStaked = false;
+
+
+        IERC20(tokenAddress).transfer(msg.sender, amount);
+
+    }
+
+    function calcReward () internal view returns(uint256){
+        if (stakers[msg.sender].isStaked == false){revert NotStaked();}
+        uint256 stakingDuration = stakers[msg.sender].startTime - stakers[msg.sender].startTime;
+        uint256 rewardRatePerSecond = 1; 
+        return stakingDuration * rewardRatePerSecond * stakers[msg.sender].amount;
+
+    }
+
+    function totalStake() external view returns(uint256){
+        if(msg.sender != owner){revert NotOwner();}
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        return totalStaked;
+    }
+
+    function checkUserStake (address _user) external view returns(uint256){
+        if(msg.sender != owner){revert NotOwner();}
+        if(msg.sender == address(0)){revert AddressZeroDetected();}
+        if(stakers[_user].isStaked == false){
+            return 0;
+        }else{
+            return stakers[_user].amount;
+        }
+    }
+}
